d2c19f4981e5cbfc7ce334ba6e0c2ead66bd9d25
Enrique Bonilla <ebonillau@gmail.com>
2021-05-27 01:42:05 -0500

    Added second NET Api send data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+    func post(with request: URLRequest, completionHandler: @escaping(Data?, Error?) -> Void)
 }
 
 extension URLSession: NetworkSession {
@@ -19,7 +20,12 @@ extension URLSession: NetworkSession {
         task.resume()
     }
     
-    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: request) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
 }
 
 extension RazeCore {
@@ -40,6 +46,26 @@ extension RazeCore {
                 session.get(from: url) { (data, error) in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
+                }
+            }
+            
+            /// Calls to send data to spec location
+            /// - Parameters:
+            ///   - url: The location you wish to send data
+            ///   - body: The object you send
+            ///   - completionHandler: Returns result
+            public func sendData<I: Codable>(to url: URL, body: I, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                var request = URLRequest(url: url)
+                do {
+                    let httpBody = try JSONEncoder().encode(body)
+                    request.httpBody = httpBody
+                    request.httpMethod = "POST"
+                    session.post(with: request) { (data, error) in
+                        let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                        completionHandler(result)
+                    }
+                } catch let error {
+                    return completionHandler(.failure(error))
                 }
             }
         }
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -15,7 +15,15 @@ class NetworkSessionMock: NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         completionHandler(data, error)
     }
+    
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
 
+struct MockData: Codable, Equatable {
+    var id: Int
+    var name: String
 }
 
 final class RazeNetworkingTests: XCTestCase {
@@ -42,7 +50,31 @@ final class RazeNetworkingTests: XCTestCase {
         wait(for: [expectation], timeout: 5)
     }
     
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = RazeCore.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "David")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "Send data")
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+        
+    }
+    
     static var allTests = [
         ("testLoadDataCall", testLoadDataCall),
+        ("testSendDataCall", testSendDataCall),
     ]
 }
